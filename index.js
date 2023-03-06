a7c28929dfdc10893280af89a4c334b590608605
Yeoungmin Shin <yshin22@gmu.edu>
2023-03-06 12:42:05 -0500

    navbar animation

---
+++ index.js
@@ -0,0 +1,5 @@
+function toggleAction(button) {
+    button.classList.toggle("active");      
+    document.getElementById("logo1").classList.toggle("active");
+    document.getElementById("logo2").classList.toggle("active");
+  }
